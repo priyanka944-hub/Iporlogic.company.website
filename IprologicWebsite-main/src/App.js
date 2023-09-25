39d8b51c140a4333e3cfa0d8ec99af3993d31af6
priyanka944-hub <ypriyapriyanka1@gmail.com>
2023-09-25 16:38:39 +0530

    changes done

---
+++ IprologicWebsite-main/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;

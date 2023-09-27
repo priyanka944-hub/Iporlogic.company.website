efc8fc63c51009a4f41df05cf798dc093e50f6ff
sandhya454 <tsandhya2014@gmail.com>
2023-09-27 14:03:03 +0530

    created footer componet

--- IprologicWebsite-main/src/Components/Pages/AboutPages/OurCompany.jsx
+++ IprologicWebsite-main/src/Components/Pages/AboutPages/OurCompany.jsx
@@ -10,6 +10,7 @@ import team4 from '../../Assets/team-4.jpg'
 import team5 from '../../Assets/team-5.jpg'
 import team6 from '../../Assets/team-6.jpg'
 import {  AwardFill, ChatSquare, Check, Flag, } from 'react-bootstrap-icons';
+import Footer from '../../Footer';
 
 const OurCompany = () => {
   return (
@@ -232,41 +233,7 @@ const OurCompany = () => {
               </div>
         </div>
 
-        <div className='last'>
-        <div className='row'>
-          <div className='col-3 bos'>
-                <h4>Company</h4>
-                <p>Terms & Conditions</p>
-                <p>Privacy Policy</p>
-                <p>Support</p>
-                <p>FAQ</p>
-          </div>
-          <div className='col-3 bos'>
-                  <h4>Quick Links</h4>
-                  <p>About</p>
-                  <p>Servieces</p>
-                  <p>Blogs</p>
-                  <p>Contact  </p>
-          
-          </div>
-          <div className='col-3 boss'>
-                  <h4>Subscribe Us</h4>
-                  <p>Subscribe to get latest news article and resources</p>
-                  <input type='text' className='form-control' placeholder='Subscribe Now'/>
-                  <button className='btn'>SUBSCRIBE</button>
-          </div>
-          <div className='col-3 boo '>
-
-          <h4>Iprologic<span>Solutions</span></h4>
-          <p><b>Support@iprologic.com</b></p>
-          <h5><b>+23-456-6588</b></h5>
-          
-          </div>
-        
-        </div>
-
-</div>
-
+<Footer/>
 
 
 
